4458e6e437e13252fd873abd21ce98d634e64c6e
CamAe09 <117052996+CamAe09@users.noreply.github.com>
2025-12-05 20:27:18 -0500

    gg

--- Assets/Scripts/TeamSystem/Revive/ReviveIntegration.cs
+++ Assets/Scripts/TeamSystem/Revive/ReviveIntegration.cs
@@ -8,6 +8,34 @@ namespace TPSBR
         [SerializeField]
         private bool _enableReviveSystem = true;
 
+        private void Update()
+        {
+            if (!_enableReviveSystem || Context == null || Context.NetworkGame == null)
+                return;
+
+            CheckAndAddReviveSystemToPlayers();
+        }
+
+        private void CheckAndAddReviveSystemToPlayers()
+        {
+            var players = Context.NetworkGame.ActivePlayers;
+            if (players == null)
+                return;
+
+            foreach (var player in players)
+            {
+                if (player == null)
+                    continue;
+
+                var reviveSystem = player.GetComponent<ReviveSystem>();
+                if (reviveSystem == null)
+                {
+                    reviveSystem = player.gameObject.AddComponent<ReviveSystem>();
+                    Debug.Log($"[ReviveIntegration] Added ReviveSystem to player {player.Nickname}");
+                }
+            }
+        }
+
         public void SetReviveSystemEnabled(bool enabled)
         {
             _enableReviveSystem = enabled;
